fdf81641b2c1148cfea023a29ccb408c784f271b
Ivan Angelovski <ivan.angelovski@intecsystem.com>
2022-08-28 18:57:28 +0200

    spring boot template

---
+++ src/main/java/com/duck/etiketi/EtiketiApplication.java
@@ -0,0 +1,13 @@
+package com.duck.etiketi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiketiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtiketiApplication.class, args);
+	}
+
+}
